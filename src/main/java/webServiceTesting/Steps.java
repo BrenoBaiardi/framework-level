585773e4695f528b2c2882b54a26a7184b55784e
Breno Baiardi <brenocbo@gmail.com>
2021-04-02 13:31:50 -0300

    code cleanup

--- src/main/java/webServiceTesting/Steps.java
+++ src/main/java/webServiceTesting/Steps.java
@@ -4,20 +4,12 @@ package webServiceTesting;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import java.net.MalformedURLException;
 
 import static io.restassured.RestAssured.*;
 
 public class Steps {
 
-    String BASE_URL = "https://reqres.in/api/users";
     CreateUser createUser;
-//    String name, job;
-
-    public Steps() throws MalformedURLException {
-    }
 
     @Given("^I use user creation service$")
     public void useUserCreationWebService() {
@@ -27,13 +19,11 @@ public class Steps {
     @When("^I set name \"([^\"]*)\"$")
     public void setName(String name) {
         createUser.setName(name);
-//        this.name = name;
     }
 
     @When("^I set job \"([^\"]*)\"$")
     public void setJob(String job) {
         createUser.setJob(job);
-//        this.job = job;
     }
 
     @Then("^I validate my response is correct$")
