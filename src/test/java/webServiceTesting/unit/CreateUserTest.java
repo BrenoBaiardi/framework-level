65987d1d3186acff67b1642e56b484a365e7b5fc
Breno Baiardi <brenocbo@gmail.com>
2021-04-03 11:08:21 -0300

    adding JSONAssert Library to manage Json tests and asserts

--- src/test/java/webServiceTesting/unit/CreateUserTest.java
+++ src/test/java/webServiceTesting/unit/CreateUserTest.java
@@ -1,10 +1,21 @@
 package webServiceTesting.unit;
 
-import org.junit.Assert;
+import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import webServiceTesting.CreateUser;
 
+
+/**
+ * Uses {@link JSONAssert} Library to validate JSON resuts
+ * <p>
+ * The tests bellow uses the {@link JSONCompareMode#LENIENT} parameter so it only tests
+ * the presence of the required fields and therefore does not fail if more than the provided fields exists</p>
+ * <p>The use of Lenient comparison provides the possibility of growth without breaking the existing tests
+ * </p>
+ */
 public class CreateUserTest {
 
   CreateUser createUser = new CreateUser();
@@ -21,18 +32,18 @@ public class CreateUserTest {
   @Before
   public void setup() {
     createUser.setName("testName");
+    createUser.setSurName("autoSurname");
     createUser.setJob("testJob");
   }
 
   @Test
-  public void buildBody_validUserAndJob_shouldReturnJsonWithUserAndJob() {
-    System.out.println(createUser.buildBody());
-    Assert.assertEquals(jsonWithUserAndJob, createUser.buildBody());
+  public void buildBody_validUserAndJob_shouldReturnJsonWithUserAndJob() throws JSONException {
+    JSONAssert.assertEquals(jsonWithUserAndJob, createUser.buildBody().toString(), JSONCompareMode.LENIENT);
   }
 
   @Test
-  public void buildBodyWithSurname_validUserAndJob_shouldReturnJsonWithUserNameSurnameAndJob() {
-    Assert.assertEquals(jsonWithUserNameSurnameAndJob, createUser.buildBodyWithSurname());
+  public void buildBodyWithSurname_validUserAndJob_shouldReturnJsonWithUserNameSurnameAndJob() throws JSONException {
+    JSONAssert.assertEquals(jsonWithUserNameSurnameAndJob, createUser.buildBodyWithSurname().toString(), JSONCompareMode.LENIENT);
   }
 
 }
